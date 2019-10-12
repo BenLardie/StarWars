f7752a610ac8465704f5175730bd50985c445013
Ben Lardie <benlardie@gmail.com>
2019-10-12 13:09:02 -0400

    infinty scroll is actually working can i get an ahmen

--- src/Componets/CharacterList.js
+++ src/Componets/CharacterList.js
@@ -1,11 +1,10 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useCallback  } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { fetchPeople } from '../actions/peopleActions'
 import { fetchStarships } from '../actions/starshipActions'
 import { Link } from 'react-router-dom'
 
 // need an if statement if url is null dont fetch
-// need to work on infinity scroll and and if to that for loading so it doesnt fetch twice
 // style
 
 
@@ -13,31 +12,29 @@ const CharacterList = () => {
 
     const people = useSelector(state => state.people.people)
     const loading = useSelector(state => state.people.loading)
+    const url = useSelector(state => state.people.url)
     const starshipLoading = useSelector(state => state.starships.loading)
     const starships = useSelector(state => state.starships.starships)
     const dispatch = useDispatch()
-    const table = document.getElementById('characters')
+    const tableRef = useRef(null)
+    const observer = useRef()
+    
+    const lastNameRow = useCallback(node => {
+        if(loading)return
+        if(observer.current) observer.current.disconnect()
+        observer.current = new IntersectionObserver(entries => {
+            if(entries[0].isIntersecting && url ){
+                dispatch(fetchPeople())
+            }
+        })
+        if (node) observer.current.observe(node)
+    },[loading, url, dispatch])
 
 
     useEffect(() => {
         dispatch(fetchPeople())
         dispatch(fetchStarships())
     }, [dispatch]);
-
-
-
-    // useEffect(() => {
-    //     const table = document.getElementById('characters')
-    //     table.addEventListener('scroll', handleScroll);
-    //     return () => table.removeEventListener('scroll', handleScroll);
-    // });
-
-    // function handleScroll() {
-    //     if (table.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) return;
-    //     dispatch(fetchPeople());
-    // }
-
-
 
     return (
         <div>
@@ -61,7 +58,27 @@ const CharacterList = () => {
                     <td>{name}</td>
                 </tr>
             )
-        } else {
+        }
+        if(people.length === i + 1){
+            const { name, birth_year, height, mass, url } = person
+            return (
+                <tr key={i} ref={lastNameRow}>
+                <td>{i + 1}</td>
+                <td>
+                <Link to={{
+                    pathname: `/viewdetials/${name}`,
+                    state: {
+                        url: {url}
+                    }
+                }}>
+                {name}
+                </Link>
+                </td>
+                <td>{birth_year}</td>
+                <td>{height}</td>
+                <td>{mass}</td>
+            </tr>
+            ) } else {
             const { name, birth_year, height, mass, url } = person
             return (
                 <tr key={i}>
