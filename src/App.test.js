4ee46eca039eb17449e65892600596ac88defafd
Ben Lardie <benlardie@gmail.com>
2019-10-15 10:35:00 -0400

    wrote another test and cleaned somethings up like the README but it's all done :raised_hands::tada::tada:

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,8 @@ import renderer from 'react-test-renderer'
 import App from './App'
 import { Provider } from 'react-redux'
 import store from './store'
+import Title from '../src/Components/Title'
+import peopleReducer from './reducers/peopleReducer'
 
 
 describe('Renders whole app with provider and compares it to a snapshot', () => {
@@ -13,5 +15,27 @@ describe('Renders whole app with provider and compares it to a snapshot', () => 
     </Provider>)
     let tree = component.toJSON()
     expect(tree).toMatchSnapshot()
+  })
+})
+
+describe('Title Snapshot test', () => {
+  test('snapshot renders', ()=> {
+    const component = renderer.create(<Title text={'test'} />)
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
+
+
+describe('people reducer', () => {
+  it('should return the intial state', () => {
+    peopleReducer(undefined, {})
+    expect.objectContaining({
+      people: [],
+      loading: false,
+      url: 'https://swapi.co/api/people/',
+      previous_url: 'https://swapi.co/api/people/?page=1', 
+      error: null
+    })
   })
 })
